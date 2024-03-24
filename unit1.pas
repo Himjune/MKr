9f5d3f98a7e139b6aecde452de0d59c5fdfc07cb
Himjune <himjune@mail.ru>
2024-03-25 00:49:52 +0300

    basic idle animation

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,19 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Image1: TImage;
+    ImageList1: TImageList;
+    ImageList2: TImageList;
+    Timer1: TTimer;
+    procedure Image1Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
 
   public
@@ -21,6 +30,25 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Image1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  if (Image1.ImageIndex-1 >= 0) then
+  begin
+     Image1.ImageIndex := Image1.ImageIndex - 1;
+  end
+  else
+  begin
+      Image1.ImageIndex := Image1.Images.Count-1;
+  end;
+end;
 
 end.
 
